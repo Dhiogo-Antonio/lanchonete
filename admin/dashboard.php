550bfd143865e8da9910e0e7e6b04e85a8f85cfb
Dhiogo-Antonio <dhiogo.marestoni@gmail.com>
2026-04-08 10:26:05 -0300

    admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -11,7 +11,7 @@ $usuario = $_SESSION['usuario'];
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
